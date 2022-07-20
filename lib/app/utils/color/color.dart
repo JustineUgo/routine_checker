d438ddafb9fa7b3681a3bbf402d0dfb12bba0c89
unknown <justineugoogaraku@gmail.com>
2022-07-20 10:35:58 +0100

    chore: setup color and theme of the app

---
+++ lib/app/utils/color/color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+Color black = const Color(0xff1e1e1e);
+Color white = const Color(0xffffffff);  
+Color backgroundPrimary = const Color(0xff252526);  
+Color backgroundSecondary = const Color(0xff2d2d30);  
+Color backgroundAccent = const Color(0xff3e3e42);  
+Color blue = const Color(0xff007acc);  
+Color orange = const Color(0xffFD7F20);  
+Color redOrange = const Color(0xffFC2E20);  
+Color amber = const Color(0xffFDB750);  
