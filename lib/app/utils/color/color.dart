c6961690f2a2c812505ec42be2ab707a53ba91d8
unknown <justineugoogaraku@gmail.com>
2022-07-20 10:51:53 +0100

    chore: adjusted colors

--- lib/app/utils/color/color.dart
+++ lib/app/utils/color/color.dart
@@ -2,8 +2,9 @@ import 'dart:ui';
 
 Color black = const Color(0xff1e1e1e);
 Color white = const Color(0xffffffff);  
-Color backgroundPrimary = const Color(0xff252526);  
-Color backgroundSecondary = const Color(0xff2d2d30);  
+Color backgroundPrimary = const Color(0xff121212);  
+Color backgroundSecondary = const Color(0xff252526);  
+Color backgroundSecondary1 = const Color(0xff2d2d30);  
 Color backgroundAccent = const Color(0xff3e3e42);  
 Color blue = const Color(0xff007acc);  
 Color orange = const Color(0xffFD7F20);  
